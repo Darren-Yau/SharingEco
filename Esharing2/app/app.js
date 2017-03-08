5c5262291b00ec474c0ebbcab6d9ad4e487078f2
pshaganti <pinky@Priyankas-MacBook-Air.local>
2017-03-07 22:32:40 -0800

    Cleaned up unnecessary files and folders
    Schema.sql and Data.sql are the only two sql files needed for data setup

--- Esharing2/app/app.js
+++ Esharing2/app/app.js
@@ -6,11 +6,20 @@ app.factory("services", ['$http', function($http) {
     obj.Posts_Cars_byType = function() {
         return $http.get(serviceBase + 'Posts_Cars_byType');
     };
+    obj.Posts_Houses_byType = function() {
+        return $http.get(serviceBase + 'Posts_Houses_byType');
+    };
     obj.Post_Car_User = function(postID) {
         return $http.get(serviceBase + 'Post_Car_User?id=' + postID);
     };
+    obj.Post_House_User = function(postID) {
+        return $http.get(serviceBase + 'Post_House_User?id=' + postID);
+    };
     obj.Car = function(CarID) {
         return $http.get(serviceBase + 'Car?id=' + CarID);
+    }
+    obj.House = function(HouseID) {
+        return $http.get(serviceBase + 'House?id=' + HouseID);
     }
     obj.updateCar = function(CarID, Car) {
         return $http.post(serviceBase + 'updateCar', {
@@ -20,12 +29,24 @@ app.factory("services", ['$http', function($http) {
             return status.data;
         });
     };
+    obj.updateHouse = function(HouseID, House) {
+        return $http.post(serviceBase + 'updateHouse', {
+            id: HouseID,
+            House: House
+        }).then(function(status) {
+            return status.data;
+        });
+    };
     obj.deleteCar = function(PostID, CarID) {
         return $http.delete(serviceBase + 'deleteCar?id=' + PostID + '&CarID=' + CarID).then(function(status) {
             return status.data;
         });
     };
-
+    obj.deleteHouse = function(PostID, HouseID) {
+        return $http.delete(serviceBase + 'deleteHouse?id=' + PostID + '&HouseID=' + HouseID).then(function(status) {
+            return status.data;
+        });
+    };
     obj.Posts_cars = function(UserID) {
         return $http.get(serviceBase + 'Posts_cars?id=' + UserID);
     };
@@ -51,9 +72,12 @@ app.factory("services", ['$http', function($http) {
             return results;
         });
     };
-    obj.deletePost = function(ItemID) {
-        return $http.delete(serviceBase + 'deletePost?id=' + ItemID).then(function(status) {
-            return status.data;
+    obj.insertHouse_Post = function(House, Post) {
+        return $http.post(serviceBase + 'insertHouse_Post', {
+            House: House,
+            Post: Post
+        }).then(function(results) {
+            return results;
         });
     };
 
@@ -75,35 +99,21 @@ app.factory("services", ['$http', function($http) {
         return results.data;
       });
     };
-    obj.SendMail = function(Mail){
-        return $http.post(serviceBase + sendMail,Mail);
-    };
+    obj.sendMail=function (Mail) {
+      return $http.post(serviceBase + 'sendMail', Mail).then(function (results) {
+        return results.data;
+      })
+    }
     return obj;
 }]);
 
-app.factory('AuthService', function() {
-    var userID;
-    return {
-        setUser: function(user) {
-            UserID = user;
-        },
-        islogged: function() {
-            return (userID) ? userID : false;
-        },
-        logout: function() {
-            userID = null;
-        }
-    }
-});
-
-app.controller('userLogin', function($scope, services, AuthService) {
+app.controller('userLogin', function($scope, services) {
     $scope.login = function() {
         var result = services.loginverify($scope.email, $scope.password);
         if (result == 0) {
             $scope.message = 'sorry, your email address and password are not match';
         } else {
-            AuthService.setUser($scope.email);
-            window.history.back();
+          window.history.back();
         }
     }
 });
@@ -130,7 +140,7 @@ app.controller('userSignup', function($scope, $location, services) {
     }
 });
 
-app.controller('postCtrl', function($scope, AuthService, services, $location, $route) {
+app.controller('postCtrl', function($scope, services, $location, $route) {
     var id = 1;
     services.Posts_cars(id).then(function(data) {
         $scope.Cars = data.data;
@@ -141,12 +151,10 @@ app.controller('postCtrl', function($scope, AuthService, services, $location, $r
     $scope.deleteCar = function(PostID, CarID) {
         services.deleteCar(PostID, CarID);
         $location.path('/postManage');
-        $route.reload();
     };
     $scope.deleteHouse = function(PostID, HouseID) {
         services.deleteHouse(PostID, HouseID);
         $location.path('/postManage');
-        $route.reload();
     };
 });
 
@@ -154,15 +162,105 @@ app.controller('CarsCtrl', function($scope, services) {
     services.Posts_Cars_byType().then(function(data) {
         $scope.Cars = data.data;
     });
+    $scope.addressIncludes = [];
+    $scope.makeIncludes = [];
+    $scope.colorIncludes = [];
+    $scope.includeAddress = function(address) {
+        var i = $.inArray(address, $scope.addressIncludes);
+        if (i > -1) {
+            $scope.addressIncludes.splice(i, 1);
+        } else {
+            $scope.addressIncludes.push(address);
+        }
+    };
+    $scope.includeMake = function(make) {
+        var i = $.inArray(make, $scope.makeIncludes);
+        if (i > -1) {
+            $scope.makeIncludes.splice(i, 1);
+        } else {
+            $scope.makeIncludes.push(make);
+        }
+    }
+    $scope.includeColor = function(color) {
+        var i = $.inArray(color, $scope.colorIncludes);
+        if (i > -1) {
+            $scope.colorIncludes.splice(i, 1);
+        } else {
+            $scope.colorIncludes.push(color);
+        }
+    }
+    $scope.query = function(car) {
+        if ($scope.addressIncludes.length > 0) {
+            if ($.inArray(car.Address, $scope.addressIncludes) < 0)
+                return;
+        }
+        if ($scope.makeIncludes.length > 0) {
+            if ($.inArray(car.Make, $scope.makeIncludes) < 0)
+                return;
+        }
+        if ($scope.colorIncludes.length > 0) {
+            if ($.inArray(car.Color, $scope.colorIncludes) < 0)
+                return;
+        }
+        return car;
+    }
 });
 
-app.controller('CarEditCtrl', function($scope, services, $routeParams, Car, Post, $location, Upload, AuthService) {
+app.controller('HousesCtrl', function($scope, services) {
+    services.Posts_Houses_byType().then(function(data) {
+        $scope.Houses = data.data;
+    });
+    $scope.addressIncludes = [];
+    $scope.roomIncludes = [];
+    $scope.bathIncludes = [];
+    $scope.includeAddress = function(address) {
+        var i = $.inArray(address, $scope.addressIncludes);
+        if (i > -1) {
+            $scope.addressIncludes.splice(i, 1);
+        } else {
+            $scope.addressIncludes.push(address);
+        }
+    };
+    $scope.includeRoom = function(room) {
+        var i = $.inArray(make, $scope.roomIncludes);
+        if (i > -1) {
+            $scope.roomIncludes.splice(i, 1);
+        } else {
+            $scope.roomIncludes.push(room);
+        }
+    }
+    $scope.includeBath = function(bath) {
+        var i = $.inArray(bath, $scope.bathIncludes);
+        if (i > -1) {
+            $scope.bathIncludes.splice(i, 1);
+        } else {
+            $scope.bathIncludes.push(bath);
+        }
+    }
+    $scope.query = function(house) {
+        if ($scope.addressIncludes.length > 0) {
+            if ($.inArray(house.Address, $scope.addressIncludes) < 0)
+                return;
+        }
+        if ($scope.roomIncludes.length > 0) {
+            if ($.inArray(house.Rooms, $scope.roomIncludes) < 0)
+                return;
+        }
+        if ($scope.bathIncludes.length > 0) {
+            if ($.inArray(house.Baths, $scope.bathIncludes) < 0)
+                return;
+        }
+        return house;
+    }
+});
+
+app.controller('CarEditCtrl', function($scope, services, $routeParams, Car, Post, $location, Upload) {
     var ID = ($routeParams.ID) ? parseInt($routeParams.ID) : 0;
     var CarID = ($routeParams.CarID) ? parseInt($routeParams.CarID) : 0;
     $scope.buttonText = (ID === 0) ? 'Add' : 'Update';
     var original_Car = Car.data;
     original_Car.Yr = parseInt(original_Car.Yr);
-    original_Car.Price = parseInt(original_Car.Price)
+    original_Car.Price = parseInt(original_Car.Price);
     var original_Post = Post.data;
     var img;
     $scope.Car = angular.copy(original_Car);
@@ -218,43 +316,166 @@ app.controller('CarEditCtrl', function($scope, services, $routeParams, Car, Post
             }
         }
         $location.path('/postManage');
-        $route.reload();
     }
 });
 
-app.controller('CarViewCtrl', function($scope, services, $routeParams, Car, $location, Upload, AuthService) {
+app.controller('HouseEditCtrl', function($scope, services, $routeParams, House, Post, $location, Upload) {
+    var ID = ($routeParams.ID) ? parseInt($routeParams.ID) : 0;
+    var HouseID = ($routeParams.HouseID) ? parseInt($routeParams.HouseID) : 0;
+    $scope.buttonText = (ID === 0) ? 'Add' : 'Update';
+    var original_House = House.data;
+    original_House.Rent = parseInt(original_House.Rent);
+    original_House.Sqft = parseInt(original_House.Sqft);
+    original_House.Rooms = parseInt(original_House.Rooms);
+    original_House.Baths = parseInt(original_House.Baths);
+    var original_Post = Post.data;
+    var img;
+    $scope.House = angular.copy(original_House);
+    $scope.Post = angular.copy(original_Post);
+    $scope.onFileSelect = function(file) {
+        if (!file) return;
+        img = file;
+    };
+    $scope.imgUpload = function() {
+        Upload.upload({
+            url: './partials/upload.php',
+            data: {
+                file: img,
+                'targetPath': '../houseimg/'
+            }
+        }).then(function(resp) {
+            $scope.Post.imgsrc = './houseimg/' + resp.data;
+        });
+    }
+
+    $scope.houseChange = function() {
+        return angular.equals(original_House, $scope.Car);
+    }
+    $scope.postChange = function() {
+        return angular.equals(original_Post, $scope.Post);
+    }
+    $scope.isChange = function() {
+        return $scope.houseChange() && $scope.postChange();
+    }
+    $scope.saveHouse = function() {
+        if (ID <= 0) {;
+            var today = new Date();
+            var year = today.getFullYear();
+            var month = today.getMonth() + 1;
+            if (month < 10) {
+                month = '0' + month;
+            }
+            var day = today.getDate();
+            if (day < 10) {
+                day = '0' + day;
+            }
+            $scope.Post.DateCreated = year + '-' + month + '-' + day;
+            $scope.Post.CarID = 0;
+            $scope.Post.CreatorID = 1;
+            $scope.Post.ItemType = 'car';
+            services.insertHouse_Post($scope.House, $scope.Post);
+        } else {
+            if (!$scope.houseChange()) {
+                services.updateHouse(HouseID, $scope.House);
+            }
+            if (!$scope.postChange()) {
+                services.updatePost(ID, $scope.Post);
+            }
+        }
+        $location.path('/postManage');
+    }
+});
+
+app.controller('CarViewCtrl', function($scope, services, $routeParams, Car) {
     $scope.Car = Car.data;
     $scope.sendMail = function() {
-        var Mail = [];
-        Mail.sendID = AuthService.userID;
-        Mail.recID = Car.data.ownerID;
-        $scope.title = 'I am interested in your car ' + Car.data.make + Car.data.model;
-        Mail.title = $scope.title;
-        Mail.content = $scope.content;
-        alert(Mail);
-        service.sendMail(Mail);
+        var Mail = {};
+        Mail.SendID = 1;
+        Mail.RecID = Car.data.CreatorID;
+        var datenow=new Date();
+        Mail.TimeSent = datenow.getTime();
+        Mail.Message = $scope.content;
+        Mail.Sender = 'kaichen';
+        Mail.Receiver = Car.data.Fname;
+        services.sendMail(Mail);
+    };
+});
+
+app.controller('HouseViewCtrl', function($scope, services, $routeParams, House) {
+    $scope.House = House.data;
+    $scope.sendMail = function() {
+        var Mail = {};
+        Mail.SendID = 1;
+        Mail.RecID = House.data.CreatorID;
+        var datenow=new Date();
+        Mail.TimeSent = datenow.getTime();
+        Mail.Message = $scope.content;
+        Mail.Sender = 'kaichen';
+        Mail.Receiver = House.data.Fname;
+        services.sendMail(Mail);
     };
 });
 
 app.controller('MailCtrl', function($scope, Messages, services, $location) {
   $scope.Messages=Messages;
   $scope.myInbox=function (obj) {
-    return obj.SendID==1;
-  };
-  $scope.myOutbox=function (obj) {
     return obj.RecID==1;
   };
+  $scope.myOutbox=function (obj) {
+    return obj.SendID==1;
+  };
+  $scope.Chats=[];
+  var receiverID=0;
+  var receivername='kaichen';
+  $scope.getChat=function (send,rec,sender,receiver) {
+    if(send==1){
+      receiverID=rec;
+      receivername=receiver;
+    }
+    else{
+      receiverID=send;
+      receivername=sender;
+    }
+    $scope.Chats=[];
+    for(var i=0;i<$scope.Messages.length;i++){
+      if(($scope.Messages[i].SendID==send && $scope.Messages[i].RecID==rec)
+    || ($scope.Messages[i].RecID==send && $scope.Messages[i].SendID==rec)){
+        $scope.Chats.push($scope.Messages[i]);
+      }
+    }
+    for(var j=0;j<$scope.Chats.length;j++){
+      if($scope.Chats[j].SendID==1){
+        $scope.Chats[j].style1='left clearfix admin_chat';
+        $scope.Chats[j].style2='chat-img1 pull-right';
+        $scope.Chats[j].style3='chat_time pull-left';
+        $scope.Chats[j].imgsrc='./images/me.png';
+      }
+      else{
+        $scope.Chats[j].style1='left clearfix';
+        $scope.Chats[j].style2='chat-img1 pull-left';
+        $scope.Chats[j].style3='chat_time pull-right';
+        $scope.Chats[j].imgsrc='./images/you.png';
+      }
+    }
+  };
+  $scope.sendMail = function() {
+      var Mail = {};
+      Mail.SendID = 1;
+      Mail.RecID = receiverID;
+      var datenow=new Date();
+      Mail.TimeSent = datenow.getTime();
+      Mail.Message = $scope.content;
+      Mail.Sender = 'kaichen';
+      Mail.Receiver = receivername;
+      services.sendMail(Mail);
+  };
+  $location.path('mailBox');
 });
 
 app.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
-        when('/', {
-                title: 'Items',
-                templateUrl: 'partials/Items.html',
-                controller: 'listCtrl'
-            })
-            .when('/login', {
+        when('/login', {
                 title: 'login',
                 templateUrl: 'partials/login.html',
                 controller: 'userLogin'
@@ -269,6 +490,11 @@ app.config(['$routeProvider',
                 templateUrl: 'partials/Carlist.html',
                 controller: 'CarsCtrl'
             })
+            .when('/houses', {
+                title: 'houses list',
+                templateUrl: 'partials/Houselist.html',
+                controller: 'HousesCtrl'
+            })
             .when('/Cardetail/:ID', {
                 title: 'Car Details',
                 templateUrl: 'partials/Cardetail.html',
@@ -280,17 +506,23 @@ app.config(['$routeProvider',
                     }
                 }
             })
-            .when('/houses', {
-                title: 'houses list',
-                templateUrl: 'partials/Houselist.html',
-                controller: 'HousesCtrl'
+            .when('/Housedetail/:ID', {
+                title: 'House Details',
+                templateUrl: 'partials/Housedetail.html',
+                controller: 'HouseViewCtrl',
+                resolve: {
+                    House: function(services, $route) {
+                        var postID = $route.current.params.ID;
+                        return services.Post_House_User(postID);
+                    }
+                }
             })
             .when('/postManage', {
                 title: 'Post Selection',
                 templateUrl: 'partials/PostSelection.html',
                 controller: 'postCtrl'
             })
-            .when('/editPost/:ID/:CarID', {
+            .when('/editCarPost/:ID/:CarID', {
                 title: 'Edit Car',
                 templateUrl: 'partials/CarEdit.html',
                 controller: 'CarEditCtrl',
@@ -305,8 +537,23 @@ app.config(['$routeProvider',
                     }
                 }
             })
+            .when('/editHousePost/:ID/:HouseID', {
+                title: 'Edit House',
+                templateUrl: 'partials/HouseEdit.html',
+                controller: 'HouseEditCtrl',
+                resolve: {
+                    Post: function(services, $route) {
+                        var ID = $route.current.params.ID;
+                        return services.Post(ID);
+                    },
+                    House: function(services, $route) {
+                        var HouseID = $route.current.params.HouseID;
+                        return services.House(HouseID);
+                    }
+                }
+            })
             .when('/mailBox', {
-                title: 'Edit Car',
+                title: 'Edit Mail',
                 templateUrl: 'partials/mailBox.html',
                 controller: 'MailCtrl',
                 resolve: {
@@ -314,17 +561,6 @@ app.config(['$routeProvider',
                     var UserID=1;
                     return services.Mails(UserID);
                   }
-                }
-            })
-            .when('/editHouse/:PostID', {
-                title: 'Edit House',
-                templateUrl: 'partials/HouseEdit.html',
-                controller: 'HouseCtrl',
-                resolve: {
-                    House: function(services, $route) {
-                        var PostID = $route.current.params.PostID;
-                        return services.Houses(PostID);
-                    }
                 }
             })
             .otherwise({
